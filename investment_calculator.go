ae186f26e9c98dec7d1350a089139ea9969f6071
Your Real Name <surafelmuhabaw17@gmail.com>
2026-02-14 10:10:10 +0300

    update

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,12 +1,18 @@
 package main
 import "fmt"
+import "os"
+import "errors"
+import "strconv"
 // import "math"
+
 func main(){
-// var investment float64	= 1000
-// var revenue float64
-// var expenses float64
-// var tax_rate float64
-var accountBalance float64 = 1000
+var accountBalance , err = readFile()
+if err !=nil{
+  fmt.Println("Error")
+  fmt.Println(err)
+  fmt.Println("_____________")
+  return
+}
 fmt.Println("Welcome to Go bank")
 fmt.Println("Enter Your Choice Number: ")
 fmt.Println("Press 1 to check balance: ")
@@ -23,38 +29,23 @@ if Choice == 1 {
   fmt.Print("How much do you want to deposit: ")
   fmt.Scan(&deposit)
   accountBalance = accountBalance + deposit
+    writeToFile(accountBalance)
+
   fmt.Print("Your New Balance is ", accountBalance)
 } else if (Choice==3){
   var withdraw float64
   fmt.Print("How much do you want to deposit: ")
   fmt.Scan(&withdraw)
   accountBalance = accountBalance - withdraw
+  writeToFile(accountBalance)
   fmt.Print("Your New Balance is ", accountBalance)
 } else{
   fmt.Print("Goodbye")
 }
-// fmt.Print("Enter Your Revenue: ")
-// fmt.Scan(&revenue)
+
 
 var num1, num2, num3 float64= valueGetter()
-//featureValues(num1,num2,num3)
-// fmt.Print("Enter The Expenses: ")
 
-// fmt.Scan(&expenses)
-// fmt.Print("Enter The Tax Rate: ")
-// fmt.Scan(&tax_rate)
-// earningBeforeTax := revenue-expenses
-// earningAfterTax := revenue - earningBeforeTax*(tax_rate/100)
-//  fmt.Print("Your earning before tax ")
-//  fmt.Println(earningBeforeTax);
-//  fmt.Print("Your earning after tax ")
-//   fmt.Println(earningAfterTax);
-// fmt.Println("The ratio")
-// fmt.Println(earningAfterTax/earningBeforeTax)
-// outPutText(earningAfterTax/earningBeforeTax)
-// fmt.Printf("YOUR RATIO WITH %f", earningAfterTax)
-
-// fmt.Println("this is the thing dawg")
  columbia, wow:=featureValues(num1,num2,num3)
 fmt.Printf("This is your earning after tax:%.1f\n", columbia)
 fmt.Printf("This is your ration:%.1f ",wow)
@@ -82,3 +73,19 @@ func valueGetter()(float64,float64, float64){
 return num1, num2, num3
 }
 //
+func writeToFile(account float64){
+  accountAmount := fmt.Sprint(account)
+  os.WriteFile("balance.txt", []byte(accountAmount), 0644)
+}
+func readFile() (float64,error){
+ data,err:= os.ReadFile("balance.txt")
+ if err != nil{
+  return 100 , errors.New("Failed to read file")
+ }
+ balanceText := string(data)
+ balance,err :=strconv.ParseFloat(balanceText, 64)
+ if err != nil{
+  return 100 , errors.New("Failed to convert ")
+ }
+return balance,nil
+}
